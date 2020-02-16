3882c8396b7de758cc1424b221bba38e0ac14333
KirinnBR <55963723+KirinnBR@users.noreply.github.com>
2020-02-16 12:30:43 -0300

    Updating the project to the right version

---
+++ Bruh Project/Assets/Scripts/HoleBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleBehaviour : MonoBehaviour
+{
+    private Collider parent;
+    private Transform playerTransform;
+    [SerializeField]
+    private LayerMask playerLayer;
+    [SerializeField]
+    private float holeRadius = 3f;
+    // Start is called before the first frame update
+    private Vector3 playerPosition { get { return playerTransform.position; } }
+    void Start()
+    {
+        transform.localScale = new Vector3(holeRadius, 0.01f, holeRadius);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(playerPosition, transform.position) > 40f)
+            return;
+        var colliders = Physics.OverlapSphere(transform.position, holeRadius/2, playerLayer, QueryTriggerInteraction.Ignore);
+        foreach (var col in colliders)
+        {
+            if (col.transform.Equals(playerTransform))
+            {
+                parent.enabled = false;
+                return;
+            }
+        }
+        parent.enabled = true;
+    }
+
+    public void ConfigureHole(Collider parent, Transform player, LayerMask layer)
+    {
+        this.parent = parent;
+        playerTransform = player;
+        playerLayer = layer;
+    }
+
+}
---
+++ Bruh Project/Assets/Scripts/ShootSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShootSystem : MonoBehaviour
+{
+    [SerializeField]
+    private Transform weapon;
+    [SerializeField]
+    private LayerMask wallLayer;
+    [SerializeField]
+    private LayerMask entitiesLayer;
+    [SerializeField]
+    private GameObject holePrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(weapon.position, weapon.forward, Color.red, 0f, false);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(weapon.position, weapon.forward, out RaycastHit hit, 100f, wallLayer, QueryTriggerInteraction.Ignore))
+            {
+                if (hit.transform.CompareTag("Destructable"))
+                {
+                    HoleBehaviour hole = Instantiate(holePrefab, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal)).GetComponent<HoleBehaviour>();
+                    hole.ConfigureHole(hit.collider, transform, entitiesLayer);
+                    Debug.Log("Destroyed.");
+                }
+                else
+                {
+                    Debug.Log("Non-destroyable obstacle.");
+                }
+            }
+        }
+    }
+}
